bb39e0929189a3b4b47cf30c0a4b88ed00dbc988
ke.wang <ke.wang@choumei.cn>
2015-07-29 15:55:10 +0800

    店铺

--- api/database/seeds/SalonDataSeeder.php
+++ api/database/seeds/SalonDataSeeder.php
@@ -24,10 +24,10 @@ class SalonDataSeeder extends Seeder
     		$line = fgetcsv($file_handle);
     		if($line && $s>1)
     		{
-    			if($line[5] && $line[3])
+    			if($line[0] && $line[1])
     			{
-    				$bindArr[$s]["salonname"] = iconv("gbk","UTF-8",$line[5]);//店铺名
-    				$bindArr[$s]["name"] = iconv("gbk","UTF-8",$line[3]);//商户名
+    				$bindArr[$s]["salonname"] = iconv("gbk","UTF-8",$line[0]);//店铺名
+    				$bindArr[$s]["name"] = iconv("gbk","UTF-8",$line[1]);//商户名
     			}
     		}
     		$s++;
