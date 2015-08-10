89cb06118c4fd4d1deac2c3e6a3ccd94120f5675
ke.wang <ke.wang@choumei.cn>
2015-08-10 14:46:04 +0800

    商户日志

---
+++ api/app/Listeners/MerchantEventListener.php
@@ -0,0 +1,104 @@
+<?php namespace App\Listeners;
+
+use App\Events;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+use Route;
+use Request;
+use App\Log;
+use JWTAuth;
+
+class MerchantEventListener {
+
+	/**
+	 * Create the event handler.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Handle the event.
+	 *
+	 * @param  Events  $event
+	 * @return void
+	 */
+	public function handle(Events $event)
+	{
+		//
+	}
+	
+
+	public function onExport()
+	{
+    	$operator = JWTAuth::parseToken()->authenticate();
+		$data['username'] = $operator->username;
+		$data['roles'] = $operator->roles->toArray();
+
+		foreach ($data['roles'] as $key => $value) {
+			$roles[] = $value['name'];
+		}
+		$data['roles'] = implode($roles, ',');
+		$data['operation'] = '导出商户';
+		$data['slug'] = Route::currentRouteName();
+		$data['ip'] = Request::getClientIp();
+		return Log::create($data);
+	}
+	
+	
+	public function onSave()
+	{
+		$operator = JWTAuth::parseToken()->authenticate();
+		$data['username'] = $operator->username;
+		$data['roles'] = $operator->roles->toArray();
+	
+		foreach ($data['roles'] as $key => $value) {
+			$roles[] = $value['name'];
+		}
+		$data['roles'] = implode($roles, ',');
+		$data['operation'] = '添加商户';
+		$data['slug'] = Route::currentRouteName();
+		$data['ip'] = Request::getClientIp();
+		return Log::create($data);
+	}
+	
+
+	public function onUpdate($log  = '')
+	{
+		$operator = JWTAuth::parseToken()->authenticate();
+		$data['username'] = $operator->username;
+		$data['roles'] = $operator->roles->toArray();
+	
+		foreach ($data['roles'] as $key => $value) {
+			$roles[] = $value['name'];
+		}
+		$data['roles'] = implode($roles, ',');
+		$data['operation'] = '修改商户';
+		$data['object'] = $log;
+		$data['slug'] = Route::currentRouteName();
+		$data['ip'] = Request::getClientIp();
+		return Log::create($data);
+	}
+
+	public function onDel($log = '')
+	{
+		$operator = JWTAuth::parseToken()->authenticate();
+		$data['username'] = $operator->username;
+		$data['roles'] = $operator->roles->toArray();
+	
+		foreach ($data['roles'] as $key => $value) {
+			$roles[] = $value['name'];
+		}
+		$data['roles'] = implode($roles, ',');
+		$data['operation'] = '删除商户';
+		$data['object'] = $log;
+		$data['slug'] = Route::currentRouteName();
+		$data['ip'] = Request::getClientIp();
+		return Log::create($data);
+	}
+	
+}
