5e5bd292ef457a07a9b4738d986c25a71fcf28c1
nian.zhu <nian.zhu@choumei.cn>
2015-08-10 17:00:00 +0800

    店铺结算相关的导出接口  ,  搜索中加入根据店铺编号搜索  ,店铺往来搜索时 ,  屏蔽值全为0的项

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -226,8 +226,25 @@ Route::group(['middleware' => ['jwt.auth','acl.auth']], function(){
     Route::any('shop_count/balance', array(
         'as' => 'shop_count.balance',
         'uses' => 'ShopCount\ShopCountController@balance'
-    ));    
-
+    ));  
+    
+    //转付单导出
+    Route::any('shop_count/export', array(
+    'as' => 'shop_count.export',
+    'uses' => 'ShopCount\ShopCountController@export'
+    ));
+    
+    //代收单导出
+    Route::any('shop_count/delegate_export', array(
+    'as' => 'shop_count.delegate_export',
+    'uses' => 'ShopCount\ShopCountController@delegate_export'
+        ));
+    
+    //店铺往来导出
+    Route::any('shop_count/balance_export', array(
+    'as' => 'shop_count.balance_export',
+    'uses' => 'ShopCount\ShopCountController@balance_export'
+    ));
 
 
 
