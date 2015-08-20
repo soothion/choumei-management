87f636d127312418afe1e6a48afbc0780e6769d9
yunfei.huang <yunfei.huang@choumei.cn>
2015-08-20 10:37:22 +0800

    添加主页面js注释，支持组件化开发，添加publick目录

---
+++ ui/module/public/widget/autocomplete/index.js
@@ -0,0 +1,8 @@
+﻿$(document).on('autoinput','#search',function(e,data){
+	if(data){
+		$('#salon_id').val(data.salon_id);
+		$('#merchant_id').val(data.merchant_id);
+		$('#merchantname').val(data.merchantname);
+		$('#sn').text(data.sn);
+	}
+});	
