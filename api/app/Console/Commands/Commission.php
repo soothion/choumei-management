ac75412ef10b10a5fa4e0f8cd0deff1c87bfdb7f
Vincent <soothion@sina.com>
2015-09-08 10:26:38 +0800

    佣金数据初始化

--- api/app/Console/Commands/Commission.php
+++ api/app/Console/Commands/Commission.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Order;
 use DB;
+use App\ShopCount;
 
 class Commission extends Command
 {
@@ -41,18 +42,17 @@ class Commission extends Command
      */
     public function handle()
     {
-       $this->commissionLog();  
        $this->commission();
     }
 
     /**
      * 佣金单流水
      */
-    public function commissionLog(){
+    public function commission(){
         $query = Order::leftJoin('commission_log','order.ordersn','=','commission_log.ordersn')
             ->join('salon','salon.salonid','=','order.salonid')
             ->where('commission_log.id','=',NULL)
-            ->select('order.ordersn','order.priceall','order.pay_time','order.use_time','order.salonid','salon.salonGrade');
+            ->select('order.ordersn','order.priceall','order.pay_time','order.use_time','order.salonid','salon.salonGrade','salon.merchantId');
         $count = $query->count();
         $pageSize = 10000;
         $totalPage = ceil($count/$pageSize);
@@ -75,41 +75,27 @@ class Commission extends Command
                 $data['updated_at'] = $date;
                 $data['created_at'] = $date;
                 $result = \App\CommissionLog::create($data);
-                if($result)
+                if($result){
+                    $commission = \App\Commission::where('salonid','=',$order->salonid)->where('date','=',date('Y-m-d',$order->use_time))->first();
+                    if($commission){
+                        $commission->update(['amount'=>$amount+$commission->amount]);
+                    }
+                    else{
+                        $commission = new \App\Commission;
+                        $commission->sn = $commission::getSn();
+                        $commission->salonid = $order->salonid;
+                        $commission->amount = $amount;
+                        $commission->date = $date;
+                        $now = date('Y-m-d H:i:s');
+                        $data['updated_at'] = $now;
+                        $data['created_at'] = $now;
+                        $commission->save();
+                    }
+                    ShopCount::count_bill_by_commission_money($order->salonid,$order->merchantId,$amount,'订单佣金',$date);
                     $this->info('订单'.$order->ordersn.'处理成功');
+                }                    
                 else
                     $this->error('订单'.$order->ordersn.'处理失败');
-            }
-        }
-    }
-
-
-    /**
-     *生成佣金单
-     */
-    public function commission(){
-        $salons =  \App\CommissionLog::select(DB::raw('distinct salonid as salonid'))->lists('salonid');
-        $this->info('总共'.count($salons).'家店铺待结算');
-        foreach ($salons as $key => $salon) {
-           $num = $key+1;
-           $this->info('正在结算第'.$num.'家店铺');
-           $commissionLogs = \App\CommissionLog::where('salonid','=',$salon)
-                ->select(DB::raw("DATE_FORMAT(`created_at`,'%Y-%m-%d') as date,sum(`amount`) as amount"))
-                ->groupBy('date')
-                ->get();
-            foreach ($commissionLogs as $commissionLog) {
-                $data['sn'] = \App\Commission::getSn();
-                $data['salonid'] = $salon;
-                $data['amount'] = $commissionLog->amount;
-                $data['date'] = $commissionLog->date;
-                $date = date('Y-m-d H:i:s');
-                $data['updated_at'] = $date;
-                $data['created_at'] = $date;
-                $result = \App\Commission::create($data);
-                if($result)
-                    $this->info('店铺'.$salon.'处理成功');
-                else
-                    $this->error('店铺'.$salon.'处理失败');
             }
         }
     }
