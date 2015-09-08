11aaae012840721773ae7a6cfdfa1c38fdd7dc8f
nian.zhu <nian.zhu@choumei.cn>
2015-09-08 15:01:17 +0800

    文档生成错误

--- api/app/Http/Controllers/Trans/OrderRefundController.php
+++ api/app/Http/Controllers/Trans/OrderRefundController.php
@@ -150,8 +150,6 @@ class OrderRefundController extends Controller
      *
      * @apiSuccess {Number} ids id(多个用','隔开).
      *
-     * @apiSuccessExample Success-Response: 
-     *
      *
      * @apiErrorExample Error-Response:
      *		{
