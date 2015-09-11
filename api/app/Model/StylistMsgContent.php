45695d74be2415e487ffe54ddd62c5e94d43d703
ke.wang <ke.wang@choumei.cn>
2015-09-11 10:02:07 +0800

    消息

---
+++ api/app/Model/StylistMsgContent.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\AbstractPaginator;
+use DB;
+class StylistMsgContent extends Model {
+
+	protected $table = 'stylist_msg_content';
+	
+	public $timestamps = false;
+	
+	
+}
