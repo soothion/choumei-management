8ed97884759746116da50444d3310f56f59ac41b
nian.zhu <nian.zhu@choumei.cn>
2015-09-18 09:55:00 +0800

    接受退款的返回

--- api/app/Http/Controllers/Trans/OrderRefundController.php
+++ api/app/Http/Controllers/Trans/OrderRefundController.php
@@ -268,45 +268,30 @@ class OrderRefundController extends Controller
      * @apiName export
      * @apiGroup refund
      *
-     * @apiParam {Number} key  1 店铺搜索  2 店铺编号
+     * @apiParam {Number} key  1订单号 2用户手机号 3用户臭美号   4店铺名称 
      * @apiParam {String} keyword  根据key来的关键字
-     * @apiParam {String} pay_time_min 付款最小时间 YYYY-MM-DD
-     * @apiParam {String} pay_time_max 付款最大时间 YYYY-MM-DD
-     * @apiParam {String} type 0 全部 1 付交易代收款 2 付业务投资款
-     * @apiParam {String} pay_type 0 全部 1 银行存款 2账扣支付 3现金  4支付宝 5财付通
-     * @apiParam {String} state 0 全部 1 待提交 2待审批 3待付款  4已付款
+     * @apiParam {String} refund_min_time 最小时间 YYYY-MM-DD
+     * @apiParam {String} refund_max_time 最大时间 YYYY-MM-DD
+     * @apiParam {String} state 0 全部  6待审核 7已退款 10退款中  12拒绝退款 (多个用','隔开)
      * @apiParam {Number} page 可选,页数. (从1开始)
      * @apiParam {Number} page_size 可选,分页大小.(最小1 最大500,默认20)
      * @apiParam {String} sort_key 排序的键 ['id','updated_at'(创建时间,默认),'code'(付款单号),'type'(付款类型),'pay_money'(付款金额),'cost_money'(换算消费额),'day'(付款日期)]
      * @apiParam {String} sort_type 排序的方式 ASC正序 DESC倒叙 (默认)
      *
-     * @apiSuccess {Number} total 总数据量.
-     * @apiSuccess {Number} per_page 分页大小.
-     * @apiSuccess {Number} current_page 当前页面.
-     * @apiSuccess {Number} last_page 当前页面.
-     * @apiSuccess {Number} from 起始数据.
-     * @apiSuccess {Number} to 结束数据.
-     * @apiSuccess {String} code 单号
-     * @apiSuccess {String} type 付款类型 1 付交易代收款 2 付业务投资款
-     * @apiSuccess {String} money 付款金额
-     * @apiSuccess {String} pay_type 付款方式   1 银行存款 2账扣支付 3现金  4支付宝 5财付通
-     * @apiSuccess {String} require_day 要求付款日期
-     * @apiSuccess {String} pay_day 实际付款日期
-     * @apiSuccess {String} cycle 回款周期
-     * @apiSuccess {String} cycle_day 回款日期
-     * @apiSuccess {String} cycle_money 周期回款金额
-     * @apiSuccess {String} make_user 制单人信息
-     * @apiSuccess {String} confirm_user 审批人信息
-     * @apiSuccess {String} cash_user 出纳人信息
-     * @apiSuccess {String} salon 店铺信息
-     * @apiSuccess {String} state 订单状态  1待提交 2待审批 3:待付款 4:已付款
-     * @apiSuccess {String} confirm_at 审批日期
-     *
      *
      */
     public function export()
     {
-        
+        //#@todo
+        $params = $this->parameters([
+            'key' => self::T_INT,
+            'keyword' => self::T_STRING,
+            'refund_min_time' => self::T_STRING,
+            'refund_max_time' => self::T_STRING,
+            'state' => self::T_STRING,
+        ]);
+        $items = TransactionSearchApi::searchOfRefund($params);
+        return $this->success($items);
     }
     
     /**
