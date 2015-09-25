95bca2bb7b7e1585b3fa5df9a9c3f3decb321751
ke.wang <ke.wang@choumei.cn>
2015-09-25 13:40:41 +0800

    价格规则model

---
+++ api/app/Model/SalonItemFormats.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalonItemFormats extends  Model
+{
+    protected $table = 'salon_item_formats';
+    
+    public $timestamps = false;
+    
+    
+    
+}
---
+++ api/app/Model/SalonNormsCat.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalonNormsCat extends  Model
+{
+    protected $table = 'salon_norms_cat';
+    
+    public $timestamps = false;
+    
+    
+    
+}
