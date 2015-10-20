2a2f4c6f2be0c871e888bb6feb7ae4168e5b170d
Vincent <soothion@sina.com>
2015-10-20 14:54:22 +0800

    用户列表优化

--- api/app/Http/Controllers/User/UserController.php
+++ api/app/Http/Controllers/User/UserController.php
@@ -166,11 +166,7 @@ class UserController extends Controller{
 
 		$page = isset($param['page'])?max($param['page'],1):1;
 		$page_size = isset($param['page_size'])?$param['page_size']:20;
-
-		//手动设置页数
-		AbstractPaginator::currentPageResolver(function() use ($page) {
-		    return $page;
-		});
+		$offset = ($page-1)*$page_size;
 
 		$fields = array(
 			'user.user_id',
@@ -187,17 +183,16 @@ class UserController extends Controller{
 		);
 
 		//分页
-	    $result = $query->select($fields)->paginate($page_size)->toArray();
-	    foreach ($result['data'] as $key=>$user) {
+	    $result = $query->select($fields)->take($page_size)->skip($offset)->toArray();
+	    foreach ($result as $key=>$user) {
 	    	$user['level'] = User::getLevel($user['growth']);
 	    	$user['sex'] = User::getSex($user['sex']);
 	    	$user['add_time'] = date('Y-m-d H:i:s',intval($user['add_time']));
-	    	$result['data'][$key] = $user;
+	    	$result[$key] = $user;
 	    }
-	    unset($result['next_page_url']);
-	    unset($result['prev_page_url']);
-	    $queries = DB::getQueryLog();
-	    return $this->success($result);
+	    $data['current_page'] = $page;
+	    $data['data'] = $result;
+	    return $this->success($data);
 
 	}
 
