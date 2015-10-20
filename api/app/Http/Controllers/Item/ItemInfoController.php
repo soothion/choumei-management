45e0761be81ab7cae9025a3341b641f2eb755ed6
ke.wang <ke.wang@choumei.cn>
2015-10-20 14:43:57 +0800

    项目规格添加

--- api/app/Http/Controllers/Item/ItemInfoController.php
+++ api/app/Http/Controllers/Item/ItemInfoController.php
@@ -735,16 +735,16 @@ class ItemInfoController extends Controller{
 		{
 			foreach ($value as $kt=>$vt)
 			{
-				//if(!in_array($vt, $clearVal))
-				//{
-					//$st = SalonItemFormat::where(['salonid'=>0,'format_name'=>$vt,'salon_item_formats_id'=>$formatsIdArr[self::$_typeArr[$key]]['salon_item_formats_id']])->first();
-					//if($st)
-					//	$norId = $st->salon_item_format_id;
-					//else
+				if(!in_array($vt, $clearVal))
+				{
+					$st = SalonItemFormat::where(['salonid'=>0,'format_name'=>$vt,'salon_item_formats_id'=>$formatsIdArr[self::$_typeArr[$key]]['salon_item_formats_id']])->first();
+					if($st)
+						$norId = $st->salon_item_format_id;
+					else
 						$norId = SalonItemFormat::insertGetId(['salonid'=>0,'format_name'=>$vt,'salon_item_formats_id'=>$formatsIdArr[self::$_typeArr[$key]]['salon_item_formats_id']]);
 					$attribute[$vt] = $norId;//属性数组
 					$clearVal[] = $vt;
-				//}
+				}
 			}
 		}
 		return $attribute;
