0d27b0d11a116286e2a242d59cc8c1cac71699c0
yu.li <yu.li@choumei.cn>
2015-10-28 10:07:52 +0800

    添加SalonItemType Model

---
+++ api/app/Model/SalonItemType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalonItemType extends Model {
+
+    protected $table = 'salon_itemtype';
+    public $timestamps = false;
+
+}
