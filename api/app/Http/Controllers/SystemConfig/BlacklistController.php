7d42bc70c0bda2a6feabab8c06baa629030eac94
zhenhua.zhang <zhenhua.zhang@choumei.cn>
2015-11-26 16:48:05 +0800

    debug blacklist.upload

--- api/app/Http/Controllers/SystemConfig/BlacklistController.php
+++ api/app/Http/Controllers/SystemConfig/BlacklistController.php
@@ -478,8 +478,9 @@ class BlacklistController extends Controller {
         $redisKey = 'blacklist';
         $available = 1;
         Excel::load($file->getPathname(), function($reader)use($param, &$data, &$redisKey, &$available) {
-            $reader = $reader->getSheet(0);
+            $reader = $reader->first();
             $array = $reader->toArray();
+            Log::info("blacklist upload array is", $array);
             array_shift($array);
             foreach ($array as $key => $value) {
                 if (empty($value[1]))
