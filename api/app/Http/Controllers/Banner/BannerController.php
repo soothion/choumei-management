2c8ac4a7fadc6f2d167670b9afcaa2e249b899d6
jiangang.zheng <jiangang.zheng@choumei.cn>
2015-12-01 18:27:31 +0800

    文档的更改

--- api/app/Http/Controllers/Banner/BannerController.php
+++ api/app/Http/Controllers/Banner/BannerController.php
@@ -30,7 +30,7 @@ class BannerController extends Controller {
      * @apiSuccess {String} name 'banner名称',(即项目名称)
      * @apiSuccess {String} image bnnaer图片.
      * @apiSuccess {Number} behavior  链接到哪里 1H5； 2app内部； 3无跳转',
-     * @apiSuccess {}    url  'banner链接地址',  (behavior为’1‘或‘3’ 类型为String ,behavior为'2'类型为json {"type":"home","id":1}且type只有四种类型：SPM - 半永久,FFA - 快时尚',home-美发店铺主页,artificer-专家主页,id:对应的下拉单的序号 )
+     * @apiSuccess {Json}    url  'banner链接地址',  (behavior为’1‘或‘3’ 类型为String ,behavior为'2'类型为json {"type":"home","id":1}且type只有四种类型：SPM - 半永久,FFA - 快时尚',home-美发店铺主页,artificer-专家主页,id:对应的下拉单的序号 )
      * 
      * 
      * @apiSuccessExample Success-Response:
@@ -112,7 +112,7 @@ class BannerController extends Controller {
      * @apiParam {String} name 必填,题目.
      * @apiParam {String} image 必填,bnnaer图片的路径.
      * @apiParam {Number} behavior 必填,'链接到哪里 1H5； 2app内部； 3无跳转'(单选按钮),
-     * @apiParam {}    url  'banner链接地址',  (behavior为’1‘或‘3’ 类型为String ,behavior为'2'类型为json {"type":"home","id":1}且type只有四种类型：SPM - 半永久,FFA - 快时尚',home-美发店铺主页,artificer-专家主页,id:对应的下拉单的序号 )
+     * @apiParam {Json}    url  'banner链接地址',  (behavior为’1‘或‘3’ 类型为String ,behavior为'2'类型为json {"type":"home","id":1}且type只有四种类型：SPM - 半永久,FFA - 快时尚',home-美发店铺主页,artificer-专家主页,id:对应的下拉单的序号 )
      * 
      * 
      * @apiSuccessExample Success-Response:
@@ -161,7 +161,7 @@ class BannerController extends Controller {
      * @apiParam {String} name 必填,题目.
      * @apiParam {String} image 必填,bnnaer图片的路径.
      * @apiParam {Number} behavior 必填,'链接到哪里 1H5； 2app内部； 3无跳转'(单选按钮),
-     * @apiParam {}    url  'banner链接地址',  (behavior为’1‘或‘3’ 类型为String ,behavior为'2'类型为json {"type":"home","id":1}且type只有四种类型：SPM - 半永久,FFA - 快时尚',home-美发店铺主页,artificer-专家主页,id:对应的下拉单的序号 )
+     * @apiParam {Json}    url  'banner链接地址',  (behavior为’1‘或‘3’ 类型为String ,behavior为'2'类型为json {"type":"home","id":1}且type只有四种类型：SPM - 半永久,FFA - 快时尚',home-美发店铺主页,artificer-专家主页,id:对应的下拉单的序号 )
      * 
      * 
      * @apiSuccessExample Success-Response:
