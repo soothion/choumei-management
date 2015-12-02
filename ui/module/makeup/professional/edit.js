20c80766210cab66ad6353e5d4a0830c34365483
yanping.li@choumei.cn <yanping.li@choumei.cn>
2015-12-02 18:58:47 +0800

    联调

--- ui/module/makeup/professional/edit.js
+++ ui/module/makeup/professional/edit.js
@@ -2,21 +2,25 @@
 * @Author: anchen
 * @Date:   2015-10-09 10:53:59
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-12-02 14:16:27
+* @Last Modified time: 2015-12-02 18:57:11
 */
 
 (function(){       
     if(lib.query.id){
         var promise = lib.ajat("artificer/show/"+lib.query.id+"#domid=form&tempid=form-t").render();
-        promise.done(function(data){                
-            initUploader();
+        promise.done(function(data){
+            var arr1 = [];
+            var arr2 = []; 
+            if(data.data.photo) arr1.push(JSON.parse(data.data.photo));
+            if(data.data.pageImage) arr2.push(JSON.parse(data.data.pageImage));
+            initUploader(arr1,arr2);
         });
     }else{
         lib.ajat("#domid=form&tempid=form-t").template({});   
         initUploader();         
     } 
 
-    function initUploader(arr){
+    function initUploader(arr1,arr2){
         lib.puploader.image({
             browse_button: 'personImagesUpload',
             auto_start:true,
@@ -26,7 +30,7 @@
                 ]
             },
             max_file_size:'10mb',
-            imageArray:arr,
+            imageArray:arr1,
             multi_selection:true,
             files_number:1,
             crop:true
@@ -45,13 +49,13 @@
                 ]
             },
             max_file_size:'10mb',
-            imageArray:arr,
+            imageArray:arr2,
             multi_selection:true,
             files_number:1,
             crop:true
         },function(uploader){
             uploader.bind('ImageUploaded',function(up,response){
-                createThumbnails(up,response,1,'750x500');                       
+                createThumbnails(up,response,750/500,'750x500');                       
             });
         });        
     }
@@ -70,18 +74,80 @@
                 }else{
                     up.preview($(response._this),{thumbimg:data[name]});
                 }
+                $("#form .upload-image-tip").hide();
             }
         });        
     }
 
+    $("#form").on('click','#introduceButton',function(){
+       if($("#introduceTbody tr").length < 10){
+           var template = $("#introduceTbody tr")[0].outerHTML; 
+           $("#introduceTbody").append(template);
+           var tr = $("#introduceTbody tr").last();
+           $(".control-help",tr).css("dispaly","none");
+           $("input",tr).val("");
+           $("textarea",tr).val("");
+       }
+    })
 
+    $("#form").on('click','button.del',function(){
+         if($("#introduceTbody tr").length != 1){
+             $(this).closest('tr').remove();
+         }
+    })
 
+    function checkedImage(ele){
+        var len = ele.length;
+        if(len){
+            $("#form .upload-image-tip").hide();
+        }else{
+            $("#form .upload-image-tip").show();
+        }
+        return len;
+    }
 
+    function getIntroduce (){
+        var arr = [];
+        $("#introduceTbody tr").each(function(i,item){
+            var obj = {};
+            obj['title'] = $(item).find('input').val();
+            obj['content'] = $(item).find('textarea').val();
+            arr.push(obj);
+        });
+        return arr;
+    }
+    
+    lib.Form.prototype.save = function(data){
+        var ele1 = $("#control-thumbnails1 img");
+        if(!checkedImage(ele1)) return;
+        var ele2 = $("#control-thumbnails2 img");
+        if(!checkedImage(ele2)) return;       
+        data.photo = JSON.stringify({thumbimg:ele1.attr('src'),img:ele1.data('original')});
+        data.pageImage = JSON.stringify({thumbimg:ele2.attr('src'),img:ele2.data('original')});
+        data.number = "1"+data.number;
+        data.detail = JSON.stringify(getIntroduce());
+        if(lib.query.id) data.id=lib.query.id;
+        submit(data)               
+    } 
 
-
- 
-
-
-
-
+    function submit(data){
+        $('form').attr('disabled',true);        
+        lib.ajax({
+            type: "post",
+            url : lib.query.id ? "artificer/update" :"artificer/add",
+            data: data    
+        }).done(function(data, status, xhr){
+            if(data.result == 1){
+                parent.lib.popup.result({
+                    text:"操作成功！",
+                    define:function(){
+                        history.back();
+                    }
+                });                                   
+            }
+            setTimeout(function(){
+                $('form').attr('disabled',false);
+            }, 2000);           
+        });
+    }
 })();
