5feb58ee9d246297585a0510707cdcfdbf703fe0
jiangang.zheng <jiangang.zheng@choumei.cn>
2015-12-08 13:55:29 +0800

    微调时间

--- api/app/Http/Controllers/Banner/BannerController.php
+++ api/app/Http/Controllers/Banner/BannerController.php
@@ -142,9 +142,19 @@ class BannerController extends Controller {
                 throw new ApiException('参数不齐', ERROR::BEAUTY_ITEM_ERROR);
             }
         }
-        $param['created_at'] = time();
-        $param['updated_at'] = time();
-        $query = Banner::create($param);
+        $date['type']=$param['type'];
+        $date['name']=$param['name'];
+        $date['image']=$param['image'];
+        $date['behavior']=$param['behavior'];
+        if (!empty($param['url'])) {
+            $date['url']=$param['url'];
+        }
+        if (!empty($param['salonName'])) {
+            $date['salonName']=$param['salonName'];
+        }
+        $date['created_at'] = time();
+        $date['updated_at'] = time();
+        $query = Banner::create($date);
         $id = $query->banner_id;
         if ($query) {
        //     Event::fire('banner.create','主键:'.$id);
@@ -184,10 +194,13 @@ class BannerController extends Controller {
         if (empty($param['type']) || !isset($param['name']) || !isset($param['image'])) {
             throw new ApiException('参数不齐', ERROR::BEAUTY_ITEM_ERROR);
         }
-        $param['behavior']=0;
-        $param['created_at'] = time();
-        $param['updated_at'] = time();
-        $query = Banner::create($param);
+        $date['type']=$param['type'];
+        $date['name']=$param['name'];
+        $date['image']=$param['image'];
+        $date['behavior']=0;
+        $date['created_at'] = time();
+        $date['updated_at'] = time();
+        $query = Banner::create($date);
         if ($query) {
             return $this->success();
         } else {
@@ -244,9 +257,6 @@ class BannerController extends Controller {
         if(!array_key_exists('salonName',$param)){
             $param['salonName']="";
         }
-//        $param['updated_at'] = time();
-//        Log::info("param is ",$param);
-//        $data['id']=$id;
         $data['type']=$param['type'];
         $data['image']=$param['image'];
         $data['salonName']=$param['salonName'];
