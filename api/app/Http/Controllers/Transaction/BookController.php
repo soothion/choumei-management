a5c070e1f715f6a1e6b551e8417363a62209852f
nian.zhu <nian.zhu@choumei.cn>
2015-12-09 11:16:22 +0800

    开发票的bug 修复

--- api/app/Http/Controllers/Transaction/BookController.php
+++ api/app/Http/Controllers/Transaction/BookController.php
@@ -493,7 +493,7 @@ class BookController extends Controller
             'specialistId'=> self::T_INT,
             'assistantId'=> self::T_INT,
         ]);
-        //$params['uid'] = 1;
+       // $params['uid'] = 1;
         $params['uid'] = $this->user->id;        
         $book = BookingCash::cash($id,$params);
         $custom_uid = $book['USER_ID'];
@@ -532,15 +532,14 @@ class BookController extends Controller
      *		}
      */
     public function bill($id)
-    {
-        return $this->success(['id'=>$id]);
+    {      
         $base = BookingOrder::where("ID",$id)->first();
         if(empty($base))
         {
             throw new ApiException("定妆单[{$id}]不存在或者已经被删除", ERROR::ORDER_NOT_EXIST);
         }
-        $state = $base->status;
-        if(!in_array($state,['CSD']))
+        $state = $base->STATUS;
+        if($state != "CSD")
         {
             throw new ApiException("定妆单[{$id}]状态不正确", ERROR::ORDER_STATUS_WRONG);
         }       
@@ -556,7 +555,7 @@ class BookController extends Controller
         'order_sn'=>$base->ORDER_SN,
         'created_at'=>date("Y-m-d H:i:s"),
         ]);
-        Event::fire('booking.bill',"预约号".$base->BOOKING_SN." "."订单号".$base->ORDER_SN);
+        //Event::fire('booking.bill',"预约号".$base->BOOKING_SN." "."订单号".$base->ORDER_SN);
         return $this->success(['id'=>$id]);
     }
     
