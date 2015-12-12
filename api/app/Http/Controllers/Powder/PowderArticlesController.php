2137714de890ca8e6e92abdd14c7fa855b7eb045
liang.hu <liang.hu@choumei.cn>
2015-12-12 14:01:00 +0800

    修改提示

--- api/app/Http/Controllers/Powder/PowderArticlesController.php
+++ api/app/Http/Controllers/Powder/PowderArticlesController.php
@@ -120,7 +120,11 @@ class PowderArticlesController extends Controller
         //查询种子池中活动券是否够用
         $seed = SeedPool::where(array('TYPE' => 'GSN' , 'STATUS' => 'NEW'))->count();
         if($seed < $param['nums']){
-            throw new ApiException('活动券数量超过10万，现在无法发券，目前券可使用数最多为'.$seed);
+            if($param['nums'] > 100000){
+                throw new ApiException('活动券数量超过10万，现在无法发券');
+            }else{
+                throw new ApiException('活动券总数量超过10万，现在无法发券，目前券可使用数最多为'.$seed);
+            }          
         }
         $data['name'] = $param['articleName'];
         $data['item_id'] = $param['itemId'];
