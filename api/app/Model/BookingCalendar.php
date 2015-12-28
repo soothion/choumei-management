a12d6e007ae401c10214e0a728d92f613df86c3d
nian.zhu <nian.zhu@choumei.cn>
2015-12-28 14:41:27 +0800

    bug

--- api/app/Model/BookingCalendar.php
+++ api/app/Model/BookingCalendar.php
@@ -21,9 +21,8 @@ class BookingCalendar extends Model {
 	    if(count($item_ids)<1)
 	    {
 	        return;
-	    }
-	    $items = BeautyItem::whereIn('item_id',$item_ids)->get(['beauty_id','item_id'])->toArray();
-	    $item_idx = Utils::column_to_key('item_id',$items);
+	    }	   
+	    $item_idx = BeautyItem::getItemBeautyId($item_ids);
 	    if(count(array_diff($item_ids, array_keys($item_idx)))>0)
 	    {
 	        throw new ApiException("部分项目已经不存在!",ERROR::PARAMETER_ERROR);
@@ -34,7 +33,7 @@ class BookingCalendar extends Model {
 	        $beauty_id = 1;
 	        if (isset($item_idx[$item_id]))
 	        {
-	            $beauty_id = $item_idx[$item_id]['beauty_id'];
+	            $beauty_id = $item_idx[$item_id];
 	        }
 	        self::upsert($item_id,$booking_date,$beauty_id,$change_type);
 	    }
